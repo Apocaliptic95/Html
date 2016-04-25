cb19af952f7f1e07619b87b17177a965f523c6ae
Apocaliptic95 <sebastos95@tlen.pl>
2016-04-25 22:46:43 +0200

    All Files

---
+++ Dreamweaver/js/main.js
@@ -0,0 +1,6 @@
+window.onload = function ()
+{
+	"use strict";
+	document.getElementById('content').textContent = "lololo";
+	document.getElementById('content').style.width = document.getElementById('bottom').offsetWidth + 'px';
+};
